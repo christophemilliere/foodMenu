d9443351fc132f8e155b4f1ecf62027be0e2b5b4
christophemilliere <christophemilliere93@gmail.com>
2018-05-01 17:40:04 +0200

    feat: collectionView

--- FoodWorld/Controllers/MenuController.swift
+++ FoodWorld/Controllers/MenuController.swift
@@ -8,11 +8,41 @@
 
 import UIKit
 
-class MenuController: UIViewController {
+class MenuController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let identifier = "MenuCell"
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
         title = "Les menus du monde"
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as? MenuCell{
+            cell.backgroundColor = GRAY_BLACK
+            return cell
+        }
+        
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width / 2 - 10
+        return CGSize(width: width, height: 200)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 25
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
     }
 
 }
---
+++ FoodWorld/Views/MenuCell.swift
@@ -0,0 +1,13 @@
+//
+//  MenuCell.swift
+//  FoodWorld
+//
+//  Created by christophe milliere on 01/05/2018.
+//  Copyright © 2018 christophe milliere. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UICollectionViewCell {
+    
+}
