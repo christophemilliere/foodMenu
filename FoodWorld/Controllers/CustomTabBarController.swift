52eda266c0b875b9885e55e6834f3b36c2ede7af
christophemilliere <christophemilliere93@gmail.com>
2018-05-01 15:54:31 +0200

    feat: tabBar custom

---
+++ FoodWorld/Controllers/CustomTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  CustomController.swift
+//  FoodWorld
+//
+//  Created by christophe milliere on 01/05/2018.
+//  Copyright © 2018 christophe milliere. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.barTintColor = GRAY_BLACK
+        tabBar.tintColor = GRAY_LIGTH
+    }
+
+}
---
+++ FoodWorld/Utilities/Constantes.swift
@@ -0,0 +1,14 @@
+//
+//  Constantes.swift
+//  FoodWorld
+//
+//  Created by christophe milliere on 01/05/2018.
+//  Copyright © 2018 christophe milliere. All rights reserved.
+//
+
+import UIKit
+
+let BLACK = UIColor.black
+let GRAY_BLACK = UIColor(red: 30/255, green: 30/255, blue: 30/255, alpha: 1)
+let GRAY_LIGTH = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
+
